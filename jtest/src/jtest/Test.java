8651b0380623d8c637c8f700c0354a4d2b9e79cd
hbxflihua <lihua_java@163.com>
2015-07-14 17:45:28 +0800

    modify in github
    modify in github

--- jtest/src/jtest/Test.java
+++ jtest/src/jtest/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello test!");
-		
+		System.out.println("hello test in 20150714!");
 		System.out.println(System.currentTimeMillis());
 	}
 	
